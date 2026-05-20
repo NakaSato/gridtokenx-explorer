b36566d404949996d492a03a0b03a531f8abb9be
WiT <wit.chanthawat@gmail.com>
2026-05-21 01:21:07 +0700

    feat(solana): add address rewards page

---
+++ app/(solana)/address/[address]/rewards/page.tsx
@@ -0,0 +1,24 @@
+import { RewardsCard } from '@/app/(features)/accounts/components/RewardsCard';
+import getReadableTitleFromAddress, {
+  AddressPageMetadataProps,
+} from '@/app/(shared)/utils/get-readable-title-from-address';
+import { Metadata } from 'next/types';
+
+type Props = Readonly<{
+  params: Promise<{
+    address: string;
+  }>;
+}>;
+
+export async function generateMetadata(props: AddressPageMetadataProps): Promise<Metadata> {
+  const { address } = await props.params;
+  return {
+    description: `Rewards due to the address ${address} by epoch on Solana`,
+    title: `Address Rewards | ${await getReadableTitleFromAddress(props)} | Solana`
+  };
+}
+
+export default async function BlockRewardsPage({ params }: Props) {
+  const { address } = await params;
+  return <RewardsCard address={address} />;
+}
