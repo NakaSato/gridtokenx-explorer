17ba2d9f4c783dd2dde7cdd8acff71e51c715fec
WiT <wit.chanthawat@gmail.com>
2026-05-21 01:21:39 +0700

    feat(solana): add address main page

---
+++ app/(solana)/address/[address]/page.tsx
@@ -0,0 +1,44 @@
+import { TransactionHistoryCard } from '@/app/(features)/accounts/components/history/TransactionHistoryCard';
+import getReadableTitleFromAddress, {
+  AddressPageMetadataProps,
+} from '@/app/(shared)/utils/get-readable-title-from-address';
+import { Metadata } from 'next/types';
+import React, { Suspense } from 'react';
+
+type Props = Readonly<{
+  params: Promise<{
+    address: string;
+  }>;
+  searchParams: Promise<{
+    cluster: string;
+    customUrl?: string;
+  }>;
+}>;
+
+export async function generateMetadata(props: AddressPageMetadataProps): Promise<Metadata> {
+  const { address } = await props.params;
+  return {
+    description: `History of all transactions involving the address ${address} on Solana`,
+    title: `Transaction History | ${await getReadableTitleFromAddress(props)} | Solana`
+  };
+}
+
+export default async function TransactionHistoryPage({ params, searchParams }: Props) {
+  const { address } = await params;
+
+  return (
+    <Suspense
+      fallback={
+        <div className="container mt-4">
+          <div className="border-primary h-12 w-12 animate-spin rounded-full border-b-2" role="status">
+            <span className="sr-only">Loading...</span>
+          </div>
+        </div>
+      }
+    >
+      <div className="container mt-4">
+        <TransactionHistoryCard address={address} />
+      </div>
+    </Suspense>
+  );
+}
