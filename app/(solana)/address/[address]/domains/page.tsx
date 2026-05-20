3d03633a6124e346da09f887e26dca8d9ba4aa08
WiT <wit.chanthawat@gmail.com>
2026-05-21 01:20:54 +0700

    feat(solana): add address domains page

---
+++ app/(solana)/address/[address]/domains/page.tsx
@@ -0,0 +1,28 @@
+import { DomainsCard } from '@/app/(features)/accounts/components/DomainsCard';
+import getReadableTitleFromAddress, {
+  AddressPageMetadataProps,
+} from '@/app/(shared)/utils/get-readable-title-from-address';
+import { Metadata } from 'next/types';
+
+type Props = Readonly<{
+  params: Promise<{
+    address: string;
+  }>;
+  searchParams: Promise<{
+    cluster: string;
+    customUrl?: string;
+  }>;
+}>;
+
+export async function generateMetadata(props: AddressPageMetadataProps): Promise<Metadata> {
+  const { address } = await props.params;
+  return {
+    description: `Domain names owned by the address ${address} on Solana`,
+    title: `Domains | ${address} | Solana`
+  };
+}
+
+export default async function OwnedDomainsPage(props: Props) {
+  const { address } = await props.params;
+  return <DomainsCard address={address} />;
+}
