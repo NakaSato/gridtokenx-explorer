28a8cda339d27ee3158f7d7a463e695036cc1e82
WiT <wit.chanthawat@gmail.com>
2026-05-21 01:21:25 +0700

    feat(solana): add address compression page-client

---
+++ app/(solana)/address/[address]/compression/page-client.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import { ParsedAccountRenderer } from '@/app/(shared)/components/account/ParsedAccountRenderer';
+import React, { Suspense } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
+
+import { CompressedNFTInfoCard } from '@/app/(shared)/components/account/CompressedNFTInfoCard';
+import { ErrorCard } from '@/app/(shared)/components/common/ErrorCard';
+import { LoadingCard } from '@/app/(shared)/components/common/LoadingCard';
+
+type Props = Readonly<{
+  params: {
+    address: string;
+  };
+}>;
+
+function CompressionCardRenderer({
+  account,
+  onNotFound,
+}: React.ComponentProps<React.ComponentProps<typeof ParsedAccountRenderer>['renderComponent']>) {
+  return (
+    <ErrorBoundary
+      fallbackRender={({ error }) => <ErrorCard text={`Failed to load compression information: ${error.message}`} />}
+    >
+      <Suspense fallback={<LoadingCard />}>
+        {<CompressedNFTInfoCard account={account} onNotFound={onNotFound} />}
+      </Suspense>
+    </ErrorBoundary>
+  );
+}
+
+export default function CompressionPageClient({ params: { address } }: Props) {
+  return <ParsedAccountRenderer address={address} renderComponent={CompressionCardRenderer} />;
+}
