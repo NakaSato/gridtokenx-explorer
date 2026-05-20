7f9867c5c7db0de73c8ad30aee98069741d2f6ba
WiT <wit.chanthawat@gmail.com>
2026-05-21 01:21:25 +0700

    feat(solana): add address compression page

---
+++ app/(solana)/address/[address]/compression/page.tsx
@@ -0,0 +1,27 @@
+import getReadableTitleFromAddress, { AddressPageMetadataProps } from '@/app/(shared)/utils/get-readable-title-from-address';
+import { Metadata } from 'next/types';
+
+import CompressionPageClient from './page-client';
+
+type Props = Readonly<{
+  params: Promise<{
+    address: string;
+  }>;
+  searchParams: Promise<{
+    cluster: string;
+    customUrl?: string;
+  }>;
+}>;
+
+export async function generateMetadata(props: AddressPageMetadataProps): Promise<Metadata> {
+  const { address } = await props.params;
+  return {
+    description: `Information about the Compressed NFT with address ${address} on Solana`,
+    title: `Compression Information | ${address} | Solana`
+  };
+}
+
+export default async function CompressionPage(props: Props) {
+  const params = await props.params;
+  return <CompressionPageClient params={params} />;
+}
