45ab8c781cde40f56f7636501f11b402a9ff411d
WiT <wit.chanthawat@gmail.com>
2026-05-21 01:16:36 +0700

    refactor: remove old program-metadata index

--- app/entities/program-metadata/index.ts
+++
@@ -1,4 +0,0 @@
-export { useProgramMetadataIdl } from './model/useProgramMetadataIdl';
-export { useProgramMetadataSecurityTxt } from './model/useProgramMetadataSecurityTxt';
-
-export { default as ProgramMetadataProgramName } from './ui/program-name';
