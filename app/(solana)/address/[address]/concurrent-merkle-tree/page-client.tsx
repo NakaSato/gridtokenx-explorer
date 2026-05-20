6ff65fecf6a9172de3609665354852ca2a781e0f
WiT <wit.chanthawat@gmail.com>
2026-05-21 01:21:33 +0700

    feat(solana): add address concurrent-merkle-tree page-client

---
+++ app/(solana)/address/[address]/concurrent-merkle-tree/page-client.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import { ConcurrentMerkleTreeCard } from '@/app/(features)/accounts/components/ConcurrentMerkleTreeCard';
+import { ParsedAccountRenderer } from '@/app/(features)/accounts/components/ParsedAccountRenderer';
+import { SPL_ACCOUNT_COMPRESSION_PROGRAM_ID as PROGRAM_ID } from '@solana/spl-account-compression';
+import React from 'react';
+
+type Props = Readonly<{
+  params: {
+    address: string;
+  };
+}>;
+
+function ConcurrentMerkleTreeCardRenderer({
+  account,
+  onNotFound,
+}: React.ComponentProps<React.ComponentProps<typeof ParsedAccountRenderer>['renderComponent']>) {
+  const rawData = account?.data?.raw;
+  if (!rawData || account.owner.toBase58() !== PROGRAM_ID.toBase58()) {
+    return onNotFound();
+  }
+  return <ConcurrentMerkleTreeCard data={rawData} />;
+}
+
+export default function MetaplexNFTMetadataPageClient({ params: { address } }: Props) {
+  return <ParsedAccountRenderer address={address} renderComponent={ConcurrentMerkleTreeCardRenderer} />;
+}
