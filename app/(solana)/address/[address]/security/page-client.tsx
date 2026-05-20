88796018db52df936e12367f47d38979d3387587
WiT <wit.chanthawat@gmail.com>
2026-05-21 01:21:05 +0700

    feat(solana): add address security page-client

---
+++ app/(solana)/address/[address]/security/page-client.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import React from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
+
+import { ParsedAccountRenderer } from '@/app/(shared)/components/account/ParsedAccountRenderer';
+import { ErrorCard } from '@/app/(shared)/components/common/ErrorCard';
+import { SecurityCard } from '@/app/(features)/security-txt/ui/SecurityCard';
+
+type Props = Readonly<{
+  params: {
+    address: string;
+  };
+}>;
+
+function SecurityCardRenderer({
+  account,
+  onNotFound,
+}: React.ComponentProps<React.ComponentProps<typeof ParsedAccountRenderer>['renderComponent']>) {
+  const parsedData = account?.data?.parsed;
+  if (!parsedData || parsedData?.program !== 'bpf-upgradeable-loader') {
+    return onNotFound();
+  }
+  return <SecurityCard data={parsedData} pubkey={account.pubkey} />;
+}
+
+export default function SecurityPageClient({ params: { address } }: Props) {
+  return (
+    <ErrorBoundary
+      fallbackRender={({ error }) => <ErrorCard text={`Failed to load security data: ${error.message}`} />}
+    >
+      <ParsedAccountRenderer address={address} renderComponent={SecurityCardRenderer} />
+    </ErrorBoundary>
+  );
+}
