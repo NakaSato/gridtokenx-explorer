b520a53f26b84b1d73d90aa6cd819e0f4a15e51f
WiT <wit.chanthawat@gmail.com>
2026-05-21 01:21:35 +0700

    feat(solana): add address anchor-program page

---
+++ app/(solana)/address/[address]/anchor-program/page.tsx
@@ -0,0 +1,30 @@
+import getReadableTitleFromAddress, { AddressPageMetadataProps } from '@/app/(shared)/utils/get-readable-title-from-address';
+import { redirect } from 'next/navigation';
+import { Metadata } from 'next/types';
+
+type Props = Readonly<{
+  params: Promise<{
+    address: string;
+  }>;
+  searchParams: Promise<{
+    cluster: string;
+    customUrl?: string;
+  }>;
+}>;
+
+export async function generateMetadata(props: AddressPageMetadataProps): Promise<Metadata> {
+  const { address } = await props.params;
+  return {
+    description: `The Interface Definition Language (IDL) file for Anchor program at address ${address} on Solana`,
+    title: `Anchor Program IDL | ${address} | Solana`,
+  };
+}
+
+/**
+ * @deprecated This route is deprecated. Programs may have multiple IDLs.
+ * The Anchor program IDL page has been renamed to a more generic one.
+ */
+export default async function DeprecatedAnchorProgramIDLPage(props: Props) {
+  const { address } = await props.params;
+  return redirect(`/address/${address}/idl`);
+}
