5b4c35b73d5c87cc2ffc4be3a6b1b49c4e07a568
WiT <wit.chanthawat@gmail.com>
2026-05-21 01:20:49 +0700

    feat(solana): add address instructions page

---
+++ app/(solana)/address/[address]/instructions/page.tsx
@@ -0,0 +1,24 @@
+import { TokenInstructionsCard } from '@/app/(features)/accounts/components/history/TokenInstructionsCard';
+import getReadableTitleFromAddress, {
+  AddressPageMetadataProps,
+} from '@/app/(shared)/utils/get-readable-title-from-address';
+import { Metadata } from 'next/types';
+
+type Props = Readonly<{
+  params: Promise<{
+    address: string;
+  }>;
+}>;
+
+export async function generateMetadata(props: AddressPageMetadataProps): Promise<Metadata> {
+  const { address } = await props.params;
+  return {
+    description: `A list of transactions that include an instruction involving a token with address ${address} on Solana`,
+    title: `Token Instructions | ${await getReadableTitleFromAddress(props)} | Solana`
+  };
+}
+
+export default async function TokenInstructionsPage(props: Props) {
+  const { address } = await props.params;
+  return <TokenInstructionsCard address={address} />;
+}
