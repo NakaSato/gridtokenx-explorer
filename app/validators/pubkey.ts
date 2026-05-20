da4b5d0b79cd57852554ba7e21418a95598fc148
WiT <wit.chanthawat@gmail.com>
2026-05-21 01:18:10 +0700

    refactor: remove old pubkey validator

--- app/validators/pubkey.ts
+++
@@ -1,4 +0,0 @@
-import { PublicKey } from '@solana/web3.js';
-import { coerce, instance, string } from 'superstruct';
-
-export const PublicKeyFromString = coerce(instance(PublicKey), string(), value => new PublicKey(value));
