f404b84e1a4761508b217d205aba546acd2f4650
WiT <wit.chanthawat@gmail.com>
2026-05-21 01:18:08 +0700

    refactor: remove old validators index

--- app/validators/index.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/no-redeclare */
-
-import { any, Infer, string, type } from 'superstruct';
-
-export type ParsedInfo = Infer<typeof ParsedInfo>;
-export const ParsedInfo = type({
-  info: any(),
-  type: string(),
-});
