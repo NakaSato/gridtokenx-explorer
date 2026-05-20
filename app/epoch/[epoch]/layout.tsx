20ba26d887fcab053ae281f7ddbf2617e6d4ef7c
WiT <wit.chanthawat@gmail.com>
2026-05-21 01:16:41 +0700

    refactor: remove old epoch layout

--- app/epoch/[epoch]/layout.tsx
+++
@@ -1,6 +0,0 @@
-import { EpochProvider } from '@/app/(core)/providers/epoch';
-import { PropsWithChildren } from 'react';
-
-export default function EpochLayout({ children }: PropsWithChildren<Record<string, never>>) {
-  return <EpochProvider>{children}</EpochProvider>;
-}
