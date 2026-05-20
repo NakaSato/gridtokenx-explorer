04e02af78c94a8157b59dba0b37bf997bade2d0c
WiT <wit.chanthawat@gmail.com>
2026-05-21 01:20:56 +0700

    feat(solana): add address entries page

---
+++ app/(solana)/address/[address]/entries/page.tsx
@@ -0,0 +1,27 @@
+import getReadableTitleFromAddress, { AddressPageMetadataProps } from '@/app/(shared)/utils/get-readable-title-from-address';
+import { Metadata } from 'next/types';
+
+import AddressLookupTableEntriesPageClient from './page-client';
+
+type Props = Readonly<{
+  params: Promise<{
+    address: string;
+  }>;
+  searchParams: Promise<{
+    cluster: string;
+    customUrl?: string;
+  }>;
+}>;
+
+export async function generateMetadata(props: AddressPageMetadataProps): Promise<Metadata> {
+  const { address } = await props.params;
+  return {
+    description: `Entries of the address lookup table at ${address} on Solana`,
+    title: `Address Lookup Table Entries | ${address} | Solana`
+  };
+}
+
+export default async function AddressLookupTableEntriesPage(props: Props) {
+  const params = await props.params;
+  return <AddressLookupTableEntriesPageClient params={params} />;
+}
